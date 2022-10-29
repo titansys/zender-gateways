5294709e36d692951ea228887d692926b1c49ca0
Norielle Cruz <mail@noriellecruz.com>
2022-10-29 18:17:38 +0800

    feat: added telnix gateway #6

---
+++ telnyx.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Telnyx SMS Gateway
+ * @author Titan Systems
+ */
+
+define("TELNYX_GATEWAY", [
+	"apikey" => "KEY018423367F8AHJS71A7A74EDB6C20F25_of4tdzcVXFptkszhkjdsd7", // Your telnyx api key
+	"from" => "+13642220854", // Your telnyx phone number
+]);
+
+function gatewaySend($phone, $message, &$system)
+{
+	/**
+	 * Implement sending here
+	 * @return bool:true
+	 * @return bool:false
+	 */
+
+	try {
+		$send = $system->guzzle->post("https://api.telnyx.com/v2/messages", [
+			"headers" => [
+				"Accept" => "application/json",
+				"Content-Type" => "application/json",
+				"Authorization" => "Bearer " . TELNYX_GATEWAY["apikey"]
+			],
+			"json" => [
+	        	"from" => TELNYX_GATEWAY["from"],
+	        	"to" => $phone,
+	        	"text" => $message
+	        ],
+	        "allow_redirects" => true,
+	        "http_errors" => false
+		]);
+
+		return $send->getStatusCode() == 200 ? true : false;
+	} catch(Exception $e){
+		return false;
+	}
+}
